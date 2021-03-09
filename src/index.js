abdfce1750a32768a396e5b261a45cdf0e0e8870
Mateus Nadal <nadalmateus@outlook.com>
2021-03-09 20:48:23 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.send('Hello World'));
+app.get('/courses', (request, response) => {
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
+});
+
+app.post('/courses', (request, response) => {
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
+
+app.put('/courses/:id', (request, response) => {
+  return response.json(['Curso 0', 'Curso 1', 'Curso 2', 'Curso 3']);
+});
+
+app.patch('/courses/:id', (request, response) => {
+  return response.json(['Curso 0', 'Curso 01', 'Curso 2', 'Curso 3']);
+});
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json(['Curso 0', 'Curso 01']);
+});
 
 app.listen(3333);
