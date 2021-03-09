bf41b1841e8ddb3fd971df031f8604aeea43cb08
Mateus Nadal <nadalmateus@outlook.com>
2021-03-09 20:17:31 -0300

    Eslint adicionado

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => response.send('Hello World'));
 
-app.get("/", (request, response) => {
-    return response.send("Hello World");
-})
-
-app.listen(3333)
-
+app.listen(3333);
